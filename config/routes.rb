4d264bf22f95184731b42ed503c66f2ccb4c803c
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-09-27 15:53:54 -0700

    generated Tasks controller with two actions: index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
