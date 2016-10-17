0d44039dc18f28cb19d2463847e77f6a4fcb2a09
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-10-17 14:51:38 -0700

    created user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001154418) do
+ActiveRecord::Schema.define(version: 20161017214854) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "action"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20161001154418) do
     t.datetime "completed_at"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "uid",        null: false
+    t.string   "provider",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
